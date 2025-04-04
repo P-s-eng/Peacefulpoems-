6cc4c47a54d45093030730fdbc8ce13116b29637
Peace Ehi <peaceadikwu992@gmail.com>
2025-04-04 05:51:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+function display(){
+    
+    document.getElementById("pen-2").innerText="1. Poetry Writing Courses 2. Mentorship & Coaching3. Poetry Challenges & Competition 4. Community & Networking 5. Custom Poetry Services";
+}
